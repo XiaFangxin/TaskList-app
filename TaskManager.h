d8f163943b9295cdc32468036af7ce823a5b9e6f
XiaFangxin <614832292@qq.com>
2024-12-16 17:15:37 +0800

    modify TaskManager

--- TaskManager.h
+++ TaskManager.h
@@ -1,5 +1,5 @@
-#ifndef _TASKMANAGER_H
-#define _TASKMANAGER_H
+#ifndef TASKMANAGER_H_
+#define TASKMANAGER_H_
 
 #include<iostream>
 #include<string>
@@ -14,31 +14,31 @@ using namespace std;
 class Monitor;
 class User;
 
-enum Tag{
+enum Tag {
     STUDY, LIFE, WORK, OTHER_TAG
 };
 
-enum Priority{
+enum Priority {
     URGENT, IMPORTANT, OTHER_PRIORITY
 };
 
-class DateTime{
+class DateTime {
     int year;
     int month;
     int day;
     friend class Task;
     friend class TaskManager;
-public:
+ public:
     DateTime(int y = 0, int m = 0, int d = 0):
-        year(y), month(m), day(d){}
+        year(y), month(m), day(d) {}
     friend bool operator < (const DateTime&d1, const DateTime& d2);
     friend bool operator > (const DateTime&d1, const DateTime& d2);
 };
 
-DateTime defaultCreateTime = DateTime(2024,1,1);
+DateTime defaultCreateTime = DateTime(2024, 1, 1);
 DateTime defaultDueTime = DateTime(2025, 1, 1);
 
-class Task{
+class Task {
     string name;
     string description;
     DateTime createDate;
@@ -49,9 +49,9 @@ class Task{
     friend class Monitor;
     friend class UserInterface;
 
-    Task(string n = "",string d = "", 
-        Tag t = OTHER_TAG, Priority p = OTHER_PRIORITY,
-        DateTime cDate = defaultCreateTime, DateTime dDate = defaultDueTime){
+    Task(string n = "", string d = "", \
+        Tag t = OTHER_TAG, Priority p = OTHER_PRIORITY, \
+        DateTime cDate = defaultCreateTime, DateTime dDate = defaultDueTime) {
             name = n; description = d;
             tag = t; priority = p;
             createDate = cDate;
@@ -64,10 +64,10 @@ class Task{
     string getDueDate();
 };
 
-class TaskManager{
-    vector<Task> taskList;//stored in database
+class TaskManager {
+    vector<Task> taskList;  // stored in database
     vector<Task> tempList;
-    void createTask(string name,string descriptrion, 
+    void createTask(string name, string descriptrion, \
         Tag tag, Priority Priority, DateTime createDate, DateTime dueDate);
     void editTask(Task* task);
     void deleteTask(Task* task);
@@ -82,59 +82,62 @@ class TaskManager{
 };
 
 /*---TaskManager---*/
-void TaskManager::createTask(string name = "",string descriptrion = "", 
-        Tag tag = OTHER_TAG, Priority Priority = OTHER_PRIORITY,
-        DateTime createDate = defaultCreateTime, DateTime dueDate = defaultDueTime){
+void TaskManager::createTask(string name = "", string descriptrion = "", \
+        Tag tag = OTHER_TAG, Priority Priority = OTHER_PRIORITY, \
+        DateTime createDate = defaultCreateTime, \
+        DateTime dueDate = defaultDueTime) {
     Task newTask = Task(name, descriptrion, tag, Priority, createDate, dueDate);
     taskList.insert(taskList.end(), newTask);
 }
 
-void TaskManager::deleteTask(Task* task){
+void TaskManager::deleteTask(Task* task) {
     vector<Task>::iterator iter = taskList.begin();
-    while(iter != taskList.end()){
-        if(iter->name == task->name)
+    while (iter != taskList.end()) {
+        if (iter->name == task->name)
             break;
         iter++;
     }
     taskList.erase(iter);
 }
 
-vector<Task>* TaskManager::filterTasks(Tag tag){
+vector<Task>* TaskManager::filterTasks(Tag tag) {
     tempList.clear();
-    for(vector<Task>::iterator iter = taskList.begin();
-        iter != taskList.end(); iter++){
-            if(iter->tag == tag)
+    for (vector<Task>::iterator iter = taskList.begin();
+        iter != taskList.end(); iter++) {
+            if (iter->tag == tag)
                 tempList.insert(tempList.begin(), *iter);
         }
     return &tempList;
 }
 
-vector<Task>* TaskManager::filterPriority(Priority priority){
+vector<Task>* TaskManager::filterPriority(Priority priority) {
     tempList.clear();
-    for(vector<Task>::iterator iter = taskList.begin();
-        iter != taskList.end(); iter++){
-            if(iter->priority == priority)
+    for (vector<Task>::iterator iter = taskList.begin();
+        iter != taskList.end(); iter++) {
+            if (iter->priority == priority)
                 tempList.insert(tempList.begin(), *iter);
         }
     return &tempList;
 }
 
-void TaskManager::getTasksByCreateDate(){
-    sort(taskList.begin(),taskList.end(), [&](const Task& a,const Task& b)->bool{
+void TaskManager::getTasksByCreateDate() {
+    sort(taskList.begin(), taskList.end(), \
+        [&](const Task& a, const Task& b)->bool {
         return a.createDate < b.createDate;
     });
 }
 
-void TaskManager::getTasksByDueDate(){
-    sort(taskList.begin(),taskList.end(), [&](const Task& a,const Task& b)->bool{
+void TaskManager::getTasksByDueDate() {
+    sort(taskList.begin(), taskList.end(), \
+        [&](const Task& a, const Task& b)->bool {
         return a.dueDate < b.dueDate;
     });
 }
 
 /*---Task---*/
-//STUDY, LIFE, WORK, OTHER_TAG
-string Task::getTag(){
-    switch(tag){
+// STUDY, LIFE, WORK, OTHER_TAG
+string Task::getTag() {
+    switch (tag) {
         case STUDY:
             return "STUDY";
             break;
@@ -151,9 +154,9 @@ string Task::getTag(){
     return "";
 }
 
-//URGENT, IMPORTANT, OTHER_PRIORITY
-string Task::getPriority(){
-    switch(priority){
+// URGENT, IMPORTANT, OTHER_PRIORITY
+string Task::getPriority() {
+    switch (priority) {
         case URGENT:
             return "URGENT";
             break;
@@ -167,20 +170,20 @@ string Task::getPriority(){
     return "";
 }
 
-string Task::getCreateDate(){
-    return to_string(createDate.year) + "/"
-        +  to_string(createDate.month) + "/"
+string Task::getCreateDate() {
+    return to_string(createDate.year) + "/" \
+        +  to_string(createDate.month) + "/" \
         +  to_string(createDate.day);
 }
 
-string Task::getDueDate(){
-    return to_string(dueDate.year) + "/"
-        +  to_string(dueDate.month) + "/"
+string Task::getDueDate() {
+    return to_string(dueDate.year) + "/" \
+        +  to_string(dueDate.month) + "/" \
         +  to_string(dueDate.day);
 }
 
-string tag_to_string(Tag tag){
-    switch(tag){
+string tag_to_string(Tag tag) {
+    switch (tag) {
         case STUDY:
             return "STUDY";
             break;
@@ -197,8 +200,8 @@ string tag_to_string(Tag tag){
     return "";
 }
 
-string priority_to_string(Priority priority){
-    switch(priority){
+string priority_to_string(Priority priority) {
+    switch (priority) {
         case URGENT:
             return "URGENT";
             break;
@@ -212,17 +215,17 @@ string priority_to_string(Priority priority){
     return "";
 }
 
-bool operator < (const DateTime&d1, const DateTime& d2){
-    if(d1.year < d2.year) return true;
-    if(d1.year > d2.year) return false;
-    if(d1.month < d2.month) return true;
-    if(d1.month > d2.month) return false;
-    if(d1.day < d2.day) return true;
+bool operator < (const DateTime&d1, const DateTime& d2) {
+    if (d1.year < d2.year) return true;
+    if (d1.year > d2.year) return false;
+    if (d1.month < d2.month) return true;
+    if (d1.month > d2.month) return false;
+    if (d1.day < d2.day) return true;
     return false;
 }
 
-bool operator > (const DateTime&d1, const DateTime& d2){
+bool operator > (const DateTime&d1, const DateTime& d2) {
     return !(d1 < d2);
 }
 
-#endif
+#endif  // TASKMANAGER_H_
